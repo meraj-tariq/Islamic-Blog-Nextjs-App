1d0615be17000bad45a305eb6b422537e21ddbeb
meraj-tarique <meraj.tarique1618@gmail.com>
2025-04-17 23:02:24 +0500

    fixed ts config and next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: [
       "localhost",
