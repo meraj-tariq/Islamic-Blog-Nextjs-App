ffe18e3f7bc616406487ceb89453fc799abd5870
meraj-tarique <meraj.tarique1618@gmail.com>
2025-04-23 01:09:12 +0500

    fixed page routing

--- src/components/(main)/layout.tsx
+++ src/components/(main)/layout.tsx
@@ -1,9 +1,9 @@
-import MainSection from "@/app/(main)/components/MainSection";
-import Services from "@/app/(main)/components/ServiceSection";
-import Events from "@/app/(main)/components/Event";
+import MainSection from "@/components/(main)/components/MainSection";
+import Services from "@/components/(main)/components/ServiceSection";
+import Events from "@/components/(main)/components/Event";
 // import { Footer } from "@/components/layout/Footer";
 // import { Header } from "@/components/layout/Header";
-import MissionAndVissionPage from "@/app/(main)/components/MissionAndVisson";
+import MissionAndVissionPage from "@/components/(main)/components/MissionAndVisson";
 
 // export const metadata = {
 //   title: "Islamic University  Research",
